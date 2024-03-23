10487e21891603f8974e5d671fd41de04420d644
Mahdi <mahditanzim2@gmail.com>
2024-03-22 23:28:21 -0400

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,27 +4,29 @@ import User from "../components/User";
 import { Link } from "react-router-dom";
 
 function Home() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]); // set as empty array initially
 
   async function fetchUsers() {
-    const { data } = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
+    const { data } = await axios.get( // can be written as const response = await axios.get("https://jsonplaceholder.typicode.com/users"); const data = response.data;
+      // destructuring the response
+      "https://jsonplaceholder.typicode.com/users" // API endpoint
     );
-    setUsers(data);
+    setUsers(data); // set the state with the response data
     console.log(data);
   }
 
   useEffect(() => {
     setTimeout(() => {
+      // simulate a delay
       fetchUsers();
     }, 1000);
-  }, []);
+  }, []); // empty array as second argument to run only once
 
   function renderUsers() {
     return users.map((user) => (
-      <Link key={user.id} to={`/users/${user.id}`}>
+      <Link key={user.id} to={`/users/${user.id}`}> 
         <User
-          id={user.id}
+          id={user.id} // pass the user's id, name, email, and username as props to the User component
           name={user.name}
           email={user.email}
           username={user.username}
@@ -34,10 +36,10 @@ function Home() {
   }
 
   function renderSkeletonLoading() {
-    return <h1>Loading...</h1>
+    return <h1>Loading...</h1>;
   }
 
-  return <div>{users.length ? renderUsers() : renderSkeletonLoading()}</div>;
+  return <div>{users.length ? renderUsers() : renderSkeletonLoading()}</div>; // if users.length is truthy, renderUsers() is called, else renderSkeletonLoading() is called
 }
 
 export default Home;
